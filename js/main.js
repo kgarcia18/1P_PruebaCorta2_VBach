d1470778ec0b62acbc2a70b83f8ef85290c27b6f
kgarcia18 <kgarcia@scl.edu.gt>
2024-03-18 21:48:23 +0000

    cronometro

--- js/main.js
+++ js/main.js
@@ -1,55 +1,56 @@
 let root = document.querySelector("#root");
-divs_root = `
-    <div class="cronometro"></div>
-    <input type="text" class="input">
-    <button class="boton">iniciar</button>
+root.innerHTML = `
+    <div class="cronometro">00:00</div>
+    <div>
+        <input type="text" class="input1" placeholder="minutos">
+        <input type="text" class="input2" placeholder="segundos">
+    </div>
+    <div class="boton">Iniciar</div>
 `;
+let cronometro = document.querySelector(".cronometro");
+let boton = document.querySelector(".boton");
+let tiempo; // Variable para almacenar el intervalo del cronómetro
 
-root.innerHTML = divs_root;
+boton.addEventListener("click", () => {
+    let minutos = parseInt(document.querySelector(".input1").value);
+    let segundos = parseInt(document.querySelector(".input2").value);
 
+    detener_cronometro();// Detener el cronómetro actual si está en marcha
 
+    iniciar_cronometro(minutos, segundos); // Iniciar el nuevo cronómetro
+});
 
-
-
-
-
-let div_cronometro = document.querySelector(".cronometro");
-let tiempo;
-
-
-function iniciar_cronometro(minutos,segundos){
-    div_cronometro.innerHTML = `${minutos}:${segundos}`;
-
-    function actualizar(){
-        if(segundos==1 & minutos == 0){
-            clearInterval(tiempo);
-        }
-        segundos--;
-
-        if(segundos==0){
-            if(minutos>=1){
-                segundos = 60
-                minutos--;
-            }
-        }
-
-        div_cronometro.innerHTML = "";
-
-        if(segundos<10){
-            div_cronometro.innerHTML = `${minutos}:0${segundos}`;
-        }else{
-            div_cronometro.innerHTML = `0${minutos}:${segundos}`;
-        }
-
-    }
-
-    tiempo = setInterval(actualizar, 1000);
+function detener_cronometro() {
+    clearInterval(tiempo);
 }
 
-iniciar_cronometro(10, 10);
+function iniciar_cronometro(minutos, segundos) {
+    function actualizar() {
+        if (segundos <= 0 && minutos <= 0) {
+            clearInterval(tiempo);
+            cronometro.innerHTML = `00:00`;
+            return; // Terminar la ejecución de la función actualizar
+        }
 
+        if (segundos <= 0) { // Actualizar segundos y minutos
+            segundos = 59;
+            minutos--;
+        } else {
+            segundos--;
+        }
 
+        // Actualizar la visualización del cronómetro con la condición
+        if (minutos < 10 && segundos < 10) {
+            cronometro.innerHTML = '0' + minutos + ":0" + segundos;
+        } else if (minutos < 10) {
+            cronometro.innerHTML = '0' + minutos + ":" + segundos;
+        } else if (segundos < 10) {
+            cronometro.innerHTML = minutos + ":0" + segundos;
+        } else {
+            cronometro.innerHTML = minutos + ":" + segundos;
+        }
+    }
 
-
-
-
+    actualizar();
+    tiempo = setInterval(actualizar, 1000);    // Iniciar el intervalo para actualizar el cronómetro cada segundo
+}
